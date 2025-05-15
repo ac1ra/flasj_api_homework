3a409c36d44fb9e17e600b656f20368b63558573
ac1ra <glombinski@gmail.com>
2025-05-15 17:17:23 +0300

    add codes

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ def hello():
     return "Hello, Flask! First start local server."
 
 
+@app.route('/contact')
+def contact():
+    return "Contact page"
+
+
+@app.route('/about')
+def about():
+    return "About page"
+
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello, {name}"
+
+
 if __name__ == "__main__":
     app.run(debug=True)

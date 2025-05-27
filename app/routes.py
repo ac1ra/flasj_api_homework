68b81810a1f3c409d1d701f82aa8331ee1732d03
ac1ra <glombinski@gmail.com>
2025-05-27 14:56:10 +0300

    add html pages

--- app/routes.py
+++ app/routes.py
@@ -1,52 +1,17 @@
+from flask import render_template
 from app import app
 
 
-@app.route("/hello")
-def hello():
-    return "Hello, world!"
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
-@app.route("/info")
-def info():
-    return "This is an informaional page."
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
-# Задание 2: Динамические маршруты
-# Реализуйте маршрут /calc//, который возвращает сумму двух чисел.
-# Пример: /calc/3/5 вернет "The sum of 3 and 5 is 8.".
-
-# Доп.задание. Добавьте обработку ошибок для неправильных данных (например, /calc/a/b).
-
-
-@app.route('/calc/<int:first>/<int:second>')
-def calculate(first, second):
-    return f"The sum of {first} and {second} is {first+second}"
-
-
-@app.route('/calc/<string:first>/<string:second>')
-def calculate_1(first, second):
-    return f"Type error. Please start again."
-
-# Задание 3. Создайте маршрут /reverse/, который переворачивает текст.
-
-# Пример: /reverse/hello → "olleh".
-
-# Доп.задание: Для маршрута /reverse/ добавьте проверку, чтобы текст содержал хотя бы один символ.
-
-
-@app.route('/reverse/<name>')
-def reverseword(name):
-    # return checksymbol(name)
-    if len(name) > 0:
-        return f"{name[::-1]}"
-
-# Задание 4. Реализуйте маршрут /user//, возвращающий:
-# Доп.задание: Для маршрута /user// добавьте валидацию возраста (например, не допускайте возраст меньше 0).
-
-
-@app.route('/user/<name>/<age>')
-def checker(name, age):
-    if int(age) > 0:
-        return f"Hello, {name.capitalize()}. You are {age} years old."
-    else:
-        return f"Error. You entered the wrong age."
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")

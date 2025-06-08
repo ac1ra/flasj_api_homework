315f1d7ea0a2046fca0b3d9332657bdcc066840f
ac1ra <glombinski@gmail.com>
2025-06-08 19:00:51 +0300

    adding getlist and secret site

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,26 @@ def submit():
     if request.method =="POST":
         name = request.form.get("name")
         email = request.form.get("email")
-        return render_template("result.html",name=name,email=email)
+        color = request.form.get("color")
+
+        profession = request.form.get("profession")
+        hobbies = request.form.getlist("hobbies")
+        
+        level=request.form.get("level")
+        return render_template("result.html",name=name,email=email,color=color,profession=profession,hobbies=hobbies,level=level)
+    else:
+        return redirect(url_for("form"))
+    
+@app.route("/secret",methods =["POST","GET"])
+def secret():
+    if request.method =="POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        color = request.form.get("color")
+
+        profession = request.form.get("profession")
+        hobbies = request.form.get("hobbies")
+        level=request.form.get("level")
+        return render_template("secret-page.html",name=name,email=email,color=color,profession=profession,hobbies=hobbies,level=level)
     else:
         return redirect(url_for("form"))
